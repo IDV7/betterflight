a7706ffcc08b3bfe42461ee9add103ed7200157a
Teppen108 <r0886311@student.thomasmore.be>
2024-03-18 08:14:38 +0100

    pid test implementation,problems fixed

--- software/Core/Src/pid_controller.c
+++ software/Core/Src/pid_controller.c
@@ -11,11 +11,12 @@
 
 
 void get_set_points( drone_pids_t *drone_pids){
+    LOGD("Set points (in get_set_points before): %f jaw, %f roll, %f pitch", drone_pids->set_points.yaw_set_point.set_point, drone_pids->set_points.roll_set_point.set_point, drone_pids->set_points.pitch_set_point.set_point);
         set_point_calculation(&drone_pids->set_points.yaw_set_point, drone_pids->stick_outputs.yaw_stick_output);
         set_point_calculation(&drone_pids->set_points.roll_set_point, drone_pids->stick_outputs.roll_stick_output);
         set_point_calculation(&drone_pids->set_points.pitch_set_point, drone_pids->stick_outputs.pitch_stick_output);
         LOGD("Stick outputs (in get_set_points): %f yaw, %f roll , %f pitch", drone_pids->stick_outputs.yaw_stick_output, drone_pids->stick_outputs.roll_stick_output, drone_pids->stick_outputs.pitch_stick_output);
-    LOGD("Set points (in get_set_points): %f jaw, %f roll, %f pitch", drone_pids->set_points.yaw_set_point.set_point, drone_pids->set_points.roll_set_point.set_point, drone_pids->set_points.pitch_set_point.set_point);
+    LOGD("Set points (in get_set_points after): %f jaw, %f roll, %f pitch", drone_pids->set_points.yaw_set_point.set_point, drone_pids->set_points.roll_set_point.set_point, drone_pids->set_points.pitch_set_point.set_point);
 }
 
 void set_pids(drone_pids_t *drone_pids) {
@@ -31,6 +32,10 @@ void test_pid_controller(void){
     pid_controller_init(&drone_pids.pids.yaw_pid, 40, 85, 20, 0.1, 0, 150, 1000, 2000);
     pid_controller_init(&drone_pids.pids.roll_pid, 40, 85, 20, 0.1, 0, 150, 1000, 2000);
     pid_controller_init(&drone_pids.pids.pitch_pid, 40, 85, 20, 0.1, 0, 150, 1000, 2000);
+
+    limits_init(&drone_pids.set_points.yaw_set_point, 0, 150);
+    limits_init(&drone_pids.set_points.roll_set_point, 0, 150);
+    limits_init(&drone_pids.set_points.pitch_set_point, 0, 150);
 
     drone_pids.stick_outputs.yaw_stick_output = 1700;
     drone_pids.stick_outputs.roll_stick_output = 1700;
--- software/Core/Src/set_point.c
+++ software/Core/Src/set_point.c
@@ -21,12 +21,12 @@ void set_point_calculation(set_point_t *set_point, float stick_output){
     LOGD("Stick limits: %f min, %f max", set_point->stick_limits.min_stick_output, set_point->stick_limits.max_stick_output);
     //limits the stick output
     if(stick_output> 1515 && stick_output < set_point->stick_limits.max_stick_output){
-        set_point->set_point = (stick_output-1515) * set_point->imu_limits.max_imu_output;
+        set_point->set_point = ((stick_output-1000)/1000) * (set_point->imu_limits.max_imu_output-set_point->imu_limits.min_imu_output);
         LOGD("Set point: %f", set_point->set_point);
 
     }
     if(stick_output < 1485 && stick_output > set_point->stick_limits.min_stick_output){
-        set_point->set_point = (stick_output-1485) * set_point->imu_limits.max_imu_output;
+        set_point->set_point = ((stick_output-1000)/1000) * (set_point->imu_limits.min_imu_output-set_point->imu_limits.min_imu_output);
         LOGD("Set point: %f", set_point->set_point);
     }
 
