ac789798bdcd85942a96509c44ee3110fb323eed
Isaak Devos <github@isaak.fastmail.com>
2024-04-22 02:57:22 +0200

    (unfinised backup commit) Extended dshot driver to be able to send special commands (unfinised).

--- software/Core/Inc/dshot.h
+++ software/Core/Inc/dshot.h
@@ -1,12 +1,3 @@
-/*
-
- This is a DSHOT driver for stm32(f7) microcontrollers.
- Inspiration from:
-    - betaflight: https://betaflight.com/ocs/development/dshot/
-    - mokhwasomssi: https://github.com/mokhwasomssi/stm32_hal_dshot
-
-*/
-
 #ifndef BETTERFLIGHT_DSHOT_H
 #define BETTERFLIGHT_DSHOT_H
 
@@ -14,12 +5,10 @@
 #include "misc.h"
 
 
-#define TIMER_CLOCK 100000000 // 100MHz
-
-#define MOTOR_BIT_0 240
-#define MOTOR_BIT_1 480
-#define DSHOT_PSC 0
-#define DSHOT_ARR 639
+#define MOTOR_BIT_0 240 // ~33% duty cycle
+#define MOTOR_BIT_1 480 // ~66% duty cycle
+#define DSHOT_PSC 0 // prescaler
+#define DSHOT_ARR 639 // auto reload register
 
 #define DSHOT_FRAME_SIZE 16
 #define DSHOT_DMA_BUFFER_SIZE 18 // 16 data bits + 2 stop bits (reset)
@@ -28,30 +17,92 @@
 #define DSHOT_MAX_THROTTLE 2047
 #define DSHOT_RANGE (DSHOT_MAX_THROTTLE - DSHOT_MIN_THROTTLE)
 
-#define MHZ_TO_HZ(x) 			((x) * 1000000)
+#define CMD_ONLY_WHILE_STOP_RANGE 36 // only allow special commands to be send when motors if they are from 0 to 36
 
-#define DSHOT600_HZ     		MHZ_TO_HZ(12)
-#define DSHOT300_HZ     		MHZ_TO_HZ(6)
-#define DSHOT150_HZ     		MHZ_TO_HZ(3)
-
+//dshot special commands, send these values to the motor instead of the throttle value to execute the command
 typedef enum {
-    DSHOT150 = 150,
-    DSHOT300 = 300,
-    DSHOT600 = 600
-} dshot_type_t;
+    DSHOT_CMD_BEEP1 = 1, // Wait at least length of beep (260ms) before next command
+    DSHOT_CMD_BEEP2 = 2, // Wait at least length of beep (260ms) before next command
+    DSHOT_CMD_BEEP3 = 3, // Wait at least length of beep (260ms) before next command
+    DSHOT_CMD_BEEP4 = 4, // Wait at least length of beep (260ms) before next command
+    DSHOT_CMD_BEEP5 = 5, // Wait at least length of beep (260ms) before next command
+    DSHOT_CMD_ESC_INFO = 6, // Wait at least 12ms before next command
+    DSHOT_CMD_SPIN_DIRECTION_1 = 7, // Need 6x
+    DSHOT_CMD_SPIN_DIRECTION_2 = 8, // Need 6x
+    DSHOT_CMD_3D_MODE_OFF = 9, // Need 6x
+    DSHOT_CMD_3D_MODE_ON = 10, // Need 6x
+    DSHOT_CMD_SETTINGS_REQUEST = 11, // Currently not implemented
+    DSHOT_CMD_SAVE_SETTINGS = 12, // Need 6x, wait at least 35ms before next command
+    DSHOT_EXTENDED_TELEMETRY_ENABLE = 13, // Need 6x (only on EDT enabled firmware)
+    DSHOT_EXTENDED_TELEMETRY_DISABLE = 14, // Need 6x (only on EDT enabled firmware)
+    DSHOT_CMD_SPIN_DIRECTION_NORMAL = 20, // Need 6x
+    DSHOT_CMD_SPIN_DIRECTION_REVERSED = 21, // Need 6x
+    DSHOT_CMD_LED0_ON = 22,
+    DSHOT_CMD_LED1_ON = 23,
+    DSHOT_CMD_LED2_ON = 24,
+    DSHOT_CMD_LED3_ON = 25,
+    DSHOT_CMD_LED0_OFF = 26,
+    DSHOT_CMD_LED1_OFF = 27,
+    DSHOT_CMD_LED2_OFF = 28,
+    DSHOT_CMD_LED3_OFF = 29,
+    DSHOT_CMD_SIGNAL_LINE_TELEMETRY_DISABLE = 32, // Need 6x. Disables commands 42 to 47
+    DSHOT_CMD_SIGNAL_LINE_TELEMETRY_ENABLE = 33, // Need 6x. Enables commands 42 to 47
+    DSHOT_CMD_SIGNAL_LINE_CONTINUOUS_ERPM_TELEMETRY = 34, // Need 6x. Enables commands 42 to 47 and sends erpm if normal Dshot frame
+    DSHOT_CMD_SIGNAL_LINE_CONTINUOUS_ERPM_PERIOD_TELEMETRY = 35, // Need 6x. Enables commands 42 to 47 and sends erpm period if normal Dshot frame
+    DSHOT_CMD_SIGNAL_LINE_TEMPERATURE_TELEMETRY = 42, // 1°C per LSB
+    DSHOT_CMD_SIGNAL_LINE_VOLTAGE_TELEMETRY = 43, // 10mV per LSB, 40.95V max
+    DSHOT_CMD_SIGNAL_LINE_CURRENT_TELEMETRY = 44, // 100mA per LSB, 409.5A max
+    DSHOT_CMD_SIGNAL_LINE_CONSUMPTION_TELEMETRY = 45, // 10mAh per LSB, 40.95Ah max
+    DSHOT_CMD_SIGNAL_LINE_ERPM_TELEMETRY = 46, // 100erpm per LSB, 409500erpm max
+    DSHOT_CMD_SIGNAL_LINE_ERPM_PERIOD_TELEMETRY = 47 // 16us per LSB, 65520us max TBD
+} dshot_special_cmd_t;
+
+// struct to keep track of special command send count
+typedef struct {
+    dshot_special_cmd_t cmd;
+    uint8_t send_count;
+    uint16_t delayms_after_cmd;
+} dshot_cmd_info_t;
+
+// table to keep track of special command info
+dshot_cmd_info_t DSHOT_CMD_SEND_INFO[] = {
+        {DSHOT_CMD_BEEP1, 1, 260},
+        {DSHOT_CMD_BEEP2, 1, 260},
+        {DSHOT_CMD_BEEP3, 1, 260},
+        {DSHOT_CMD_BEEP4, 1, 260},
+        {DSHOT_CMD_BEEP5, 1, 260},
+        {DSHOT_CMD_ESC_INFO, 1, 12},
+        {DSHOT_CMD_SPIN_DIRECTION_1, 6, 0},
+        {DSHOT_CMD_SPIN_DIRECTION_2, 6, 0},
+        {DSHOT_CMD_3D_MODE_OFF, 6, 0},
+        {DSHOT_CMD_3D_MODE_ON, 6, 0},
+        {DSHOT_CMD_SAVE_SETTINGS, 6, 35},
+        {DSHOT_EXTENDED_TELEMETRY_ENABLE, 6, 0},
+        {DSHOT_EXTENDED_TELEMETRY_DISABLE, 6, 0},
+        {DSHOT_CMD_SPIN_DIRECTION_NORMAL, 6, 0},
+        {DSHOT_CMD_SPIN_DIRECTION_REVERSED, 6, 0},
+        {DSHOT_CMD_SIGNAL_LINE_TELEMETRY_DISABLE, 6, 0},
+        {DSHOT_CMD_SIGNAL_LINE_TELEMETRY_ENABLE, 6, 0},
+        {DSHOT_CMD_SIGNAL_LINE_CONTINUOUS_ERPM_TELEMETRY, 6, 0},
+        {DSHOT_CMD_SIGNAL_LINE_CONTINUOUS_ERPM_PERIOD_TELEMETRY, 6, 0}
+};
+#define DSHOT_CMD_SEND_INFO_SIZE (sizeof(DSHOT_CMD_SEND_INFO) / sizeof(DSHOT_CMD_SEND_INFO[0]))
+
 
 typedef struct {
     uint32_t dma_buffer[DSHOT_DMA_BUFFER_SIZE];
     TIM_HandleTypeDef *htim;
     DMA_HandleTypeDef *hdma;
     uint32_t tim_channel;
-    uint16_t motor_value;
+    uint16_t throttle_value; // last speed value set by user (altered by dshot_set_speed!!)
+    dshot_special_cmd_t special_cmd;  // last special command set by user
+    uint8_t send_special_cmd_count; // keep track of how many times a special command has been sent, if counter is more then 0 the stored special command will be send instead of the throttle value
 } dshot_handle_t;
 
 void dshot_init(dshot_handle_t *dshot_h, TIM_HandleTypeDef *htim, DMA_HandleTypeDef *hdma, uint32_t tim_channel);
 void dshot_process(dshot_handle_t *dshot_h);
 void dshot_set_speed(dshot_handle_t *dshot, uint16_t throttle);
 void dshot_stop(dshot_handle_t *dshot_h);
-void dshot_send(dshot_handle_t *dshot_h, uint16_t* motor_value);
+void dshot_send_special_command(dshot_handle_t *dshot_h, dshot_special_cmd_t cmd);
 
 #endif //BETTERFLIGHT_DSHOT_H
--- software/Core/Src/dshot.c
+++ software/Core/Src/dshot.c
@@ -1,29 +1,33 @@
+/*
+ This is a dshot driver for stm32(f7) microcontrollers that makes use of the stm32 HAL library.
+ It is almost 100% portable, you will just need to remove my project specific logging functions and includes.
+ You will also have to reconfigure the PSC and ARR values to match your peripheral clock speed.
+ */
+
 
 #include "dshot.h"
 #include "main.h"
-#include <math.h>
 #include "log.h"
-#include "misc.h"
 #include "stm32f7xx_hal_tim.h"
 
 
-static uint16_t dshot_prepare_packet(uint16_t value);
+static void dshot_prepare_packet(uint32_t *dma_buffer ,uint16_t value);
 static void dshot_dma_complete_callback(DMA_HandleTypeDef *hdma);
-uint32_t dshot_find_ccrx(dshot_handle_t *dshot_h);
+static uint32_t dshot_find_ccrx(dshot_handle_t *dshot_h);
 static void dshot_dma_enable(DMA_HandleTypeDef *hdma, bool enable);
+static void dshot_send(dshot_handle_t *dshot_h, uint16_t* motor_value);
+static dshot_cmd_info_t* dshot_find_special_cmd_info(dshot_special_cmd_t cmd);
 
 void dshot_init(dshot_handle_t *dshot_h, TIM_HandleTypeDef *htim, DMA_HandleTypeDef *hdma, uint32_t tim_channel) {
     dshot_h->htim = htim;
     dshot_h->hdma = hdma;
     dshot_h->tim_channel = tim_channel;
-    dshot_h->motor_value = 0;
+    dshot_h->throttle_value = 0;
 
     // set timer
-//    uint16_t dshot_prescaler = (lrintf((float)TIMER_CLOCK/ (float)DSHOT600_HZ + 0.01f) - 1);
     __HAL_TIM_SET_PRESCALER(dshot_h->htim, DSHOT_PSC);
     __HAL_TIM_SET_AUTORELOAD(dshot_h->htim, DSHOT_ARR);
     HAL_TIM_PWM_Start(dshot_h->htim, dshot_h->tim_channel);
-
 
     //set dma callback function
     dshot_h->hdma->XferCpltCallback = dshot_dma_complete_callback;
@@ -31,6 +35,8 @@ void dshot_init(dshot_handle_t *dshot_h, TIM_HandleTypeDef *htim, DMA_HandleType
 
 // CALL EVERY 1MS or esc will disconnect
 void dshot_process(dshot_handle_t * dshot_h) {
+
+    // warm user if dshot_process is not called every 2ms, esc might disarm if not send a new frame every so often
     static uint64_t last_time = 0;
     uint64_t current_time = millis;
     if (last_time != 0 && (current_time - last_time) > 2) {
@@ -38,12 +44,19 @@ void dshot_process(dshot_handle_t * dshot_h) {
     }
     last_time = current_time;
 
-    //command dma to send new value to motor
-    dshot_send(dshot_h, &dshot_h->motor_value);
+    //check if we have to send a special command
+    if (dshot_h->send_special_cmd_count > 0) {
+        dshot_send(dshot_h, (uint16_t*)&dshot_h->special_cmd);
+        dshot_h->send_special_cmd_count--;
+        return;
+    }
+
+    //send stored value to motor
+    dshot_send(dshot_h, &dshot_h->throttle_value);
 }
 
 void dshot_stop(dshot_handle_t *dshot_h) {
-    dshot_h->motor_value = 0;
+    dshot_h->throttle_value = 0;
 }
 
 //doesn't change the speed, expects you to call dshot_process every 1ms
@@ -54,29 +67,38 @@ void dshot_set_speed(dshot_handle_t *dshot_h, uint16_t throttle) {
         throttle = DSHOT_MAX_THROTTLE;
     }
 
-    dshot_h->motor_value = throttle - DSHOT_MIN_THROTTLE;
+    dshot_h->throttle_value = throttle - DSHOT_MIN_THROTTLE;
 }
 
+void dshot_send_special_command(dshot_handle_t *dshot_h, dshot_special_cmd_t cmd) {
+    dshot_h->special_cmd = cmd;
+    dshot_cmd_info_t *cmd_info = dshot_find_special_cmd_info(cmd);
 
-
-
-void dshot_send(dshot_handle_t *dshot_h, uint16_t *value) {
-
-    // put packet in dma buffer
-    uint16_t packet = dshot_prepare_packet(*value);
-    for (int i = 0; i < DSHOT_FRAME_SIZE; i++) {
-        dshot_h->dma_buffer[i] = (packet & 0x8000) ? MOTOR_BIT_1 : MOTOR_BIT_0;
-        packet <<= 1;
+    //refuse cmd if it requires motor to be stopped and the motor is still spinning
+    if (cmd_info->cmd (stopped here for the day) CMD_ONLY_WHILE_STOP_RANGE && dshot_h->throttle_value > 0) {
+        LOGW((uint8_t*)"Special command %d refused, motor is still spinning", cmd);
+        return;
     }
-    dshot_h->dma_buffer[16] = 0;
-    dshot_h->dma_buffer[17] = 0;
+
+
+    dshot_h->send_special_cmd_count = cmd_info->send_count;
+
+
+
+}
+
+// ---------- static functions ---------- //
+
+// raw send function, use dshot_set_speed to set motor speed, dshot_send_special_command to send special commands
+static void dshot_send(dshot_handle_t *dshot_h, uint16_t *value) {
+    dshot_prepare_packet(dshot_h->dma_buffer, *value);
 
     HAL_DMA_Start_IT(dshot_h->hdma, (uint32_t) dshot_h->dma_buffer, dshot_find_ccrx(dshot_h), DSHOT_DMA_BUFFER_SIZE);
     dshot_dma_enable(dshot_h->hdma, true);
     // when dma is done, the dma callback function will disable the dma channel.
 }
 
-static uint16_t dshot_prepare_packet(uint16_t value) {
+static void dshot_prepare_packet(uint32_t *dma_buffer ,uint16_t value) {
     uint16_t packet = 0;
     bool dshot_telemetry = false;
 
@@ -94,8 +116,16 @@ static uint16_t dshot_prepare_packet(uint16_t value) {
     csum &= 0xf;
     packet = (packet << 4) | csum;
 
-    return packet;
+    // the dma buffer represents pwm duty cycles for each bit in the frame (the values depends on PSC and ARR)
+    // put packet in dma buffer with correct pwm duty cycle
+    for (int i = 0; i < DSHOT_FRAME_SIZE; i++) {
+        dma_buffer[i] = (packet & 0x8000) ? MOTOR_BIT_1 : MOTOR_BIT_0;
+        packet <<= 1;
+    }
+    dma_buffer[16] = 0;
+    dma_buffer[17] = 0;
 }
+
 
 
 static void dshot_dma_complete_callback(DMA_HandleTypeDef *hdma) {
@@ -134,8 +164,7 @@ static void dshot_dma_enable(DMA_HandleTypeDef *hdma, bool enable) {
     }
 }
 
-
-uint32_t dshot_find_ccrx(dshot_handle_t *dshot_h) {
+static uint32_t dshot_find_ccrx(dshot_handle_t *dshot_h) {
     if (dshot_h->tim_channel == TIM_CHANNEL_1) {
         return (uint32_t)&dshot_h->htim->Instance->CCR1;
     } else if (dshot_h->tim_channel == TIM_CHANNEL_2) {
@@ -149,4 +178,17 @@ uint32_t dshot_find_ccrx(dshot_handle_t *dshot_h) {
         return 0;
     }
 }
+
+static dshot_cmd_info_t* dshot_find_special_cmd_info(dshot_special_cmd_t cmd) {
+    for (int i = 0; i < DSHOT_CMD_SEND_INFO_SIZE; i++) {
+        if (DSHOT_CMD_SEND_INFO[i].cmd == cmd) {
+            return &DSHOT_CMD_SEND_INFO[i];
+        }
+    }
+
+    // return standard info if not found, this is normal, only cmd's with special requirements are in the table
+    dshot_cmd_info_t standard_cmd_info = {cmd, 1, 0};
+    return &standard_cmd_info;
+}
+
 
