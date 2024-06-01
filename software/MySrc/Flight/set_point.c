60fb8cadf69cc19f48b610bcadd81606179f0460
Teppen108 <131367354+Teppen108@users.noreply.github.com>
2024-06-01 13:32:33 +0200

    set_point cleanup

--- software/MySrc/Flight/set_point.c
+++ software/MySrc/Flight/set_point.c
@@ -1,13 +1,7 @@
-//
-// Created by Maarten on 5-3-2024.
-//
+
 #include "set_point.h"
 
-#include <stdio.h>
 
-#include "log.h"
-#include "misc.h"
-#define MAX(a, b) (((a) > (b)) ? (a) : (b));
 
 float power5(float x) {
     return x * x * x * x * x;
@@ -23,15 +17,13 @@ void limits_init(set_point_handle_t *set_point, int16_t min_imu_output, int16_t 
 
 int16_t set_point_calculation(set_point_handle_t *set_point, int16_t stick_output,float rc_commandf, const float rc_commandfabs){
 
-    //LOGD("Stick output: %d", stick_output);
-
     float expof = set_point->actual_rates.rc_expo / 100.0f;
     expof = rc_commandfabs * (power5(rc_commandf) * expof + rc_commandf * (1 - expof));
 
     const float centerSensitivity = set_point->actual_rates.rc_rates * 10.0f;
     const float stickMovement = MAX(0, set_point->actual_rates.rates * 10.0f - centerSensitivity);
     const float angleRate = rc_commandf * centerSensitivity + stickMovement * expof;
-    //LOGD("set_point %d",(int16_t)(angleRate * (float)stick_output));
+
     return ((int16_t)(angleRate * (float)stick_output));
 
 }
--- software/MySrc/Flight/set_point.h
+++ software/MySrc/Flight/set_point.h
@@ -1,9 +1,17 @@
+/*
+ * set_point_calculation function calculates the setpoint for the PID controller based on the stick value
+ * The function uses the actual rates and stick limits to calculate the setpoint.
+ * rc_rates: center sensitivity of the stick (smaller adjustments around center, bigger adjustments at the end)
+ * rc_expo: expo
+ * rates: max rate
+ */
 #ifndef BETTERFLIGHT_SET_POINT_H
 #define BETTERFLIGHT_SET_POINT_H
 
 #include "stdint.h"
 #include "common_structs.h"
 
+#define MAX(a, b) (((a) > (b)) ? (a) : (b));
 
 typedef struct{
     float rc_rates;
@@ -16,7 +24,6 @@ typedef struct {
     stick_limits_t stick_limits;
     actual_rates_t actual_rates;
     int16_t imu_val;
-    //set_point_t setp;
 } set_point_handle_t;
 
 void limits_init(set_point_handle_t *set_point, int16_t min_imu_output, int16_t max_imu_output);
