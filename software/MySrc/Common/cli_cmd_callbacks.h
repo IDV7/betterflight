8c09f0a064c3d7686f15a310bcc5692e31742c1e
Isaak Devos <github@isaak.fastmail.com>
2024-06-01 14:22:08 +0200

    cmd cleanup

--- software/MySrc/Common/cli_cmd_callbacks.h
+++ software/MySrc/Common/cli_cmd_callbacks.h
@@ -1,12 +1,10 @@
-//
-// Created by Isaak on 2/27/2024.
-//
 
 #ifndef BETTERFLIGHT_CLI_CMD_CALLBACKS_H
 #define BETTERFLIGHT_CLI_CMD_CALLBACKS_H
 
 #include "cli.h"
 
+// this function contains all the individual command inits (should be called by cli_init)
 void add_commands(cli_handle_t *cli_h);
 
 #endif //BETTERFLIGHT_CLI_CMD_CALLBACKS_H
