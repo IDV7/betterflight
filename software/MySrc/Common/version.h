d8cbe933a80f42bf197e30468d05098a46b89b0e
Isaak Devos <github@isaak.fastmail.com>
2024-06-01 14:23:26 +0200

    version cleanup

--- software/MySrc/Common/version.h
+++ software/MySrc/Common/version.h
@@ -1,6 +1,10 @@
-//
-// Created by Isaak on 2/21/2024.
-//
+/*
+
+  it was once an idea to let the qt application check the version of the CLI module to see if it was compatible with the firmware version.
+  but it was never implemented in the end.
+
+ */
+
 
 #ifndef BETTERFLIGHT_VERSION_H
 #define BETTERFLIGHT_VERSION_H
@@ -11,7 +15,9 @@ typedef struct {
     int patch;
 } version_t;
 
+// macro to convert a version_t to a string
 #define VERSION_TO_STRING(version, version_string) sprintf((char*)version_string, "%d.%d.%d", version.major, version.minor, version.patch)
+
 
 #define CLI_VERSION (version_t){0, 1, 3}
 #define FW_VERSION (version_t){0, 1, 0}
